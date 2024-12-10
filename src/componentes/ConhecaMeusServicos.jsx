5f2df5debd859e737310f972c08d6cf939ed0ec5
Lara <laraf.sena@gmail.com>
2024-12-10 11:47:08 -0300

    ajuste

--- src/componentes/ConhecaMeusServicos.jsx
+++ src/componentes/ConhecaMeusServicos.jsx
@@ -7,11 +7,11 @@ function ConhecaMeusServicos(){
             <div className='conheca_meus_servicos_textos'>
                 <h1>Conheça meus serviços</h1>
 
-<div className='texto_p'>
+                <div className='texto_p'>
                 <p>Vejo o trabalho na clínica como uma ferramenta para acolher o sofrimento, angústia e conflitos psicológicos dos indivíduos que buscam ajuda profissional.</p>
                 <p>Atuo na clínica com demandas como: Depressão, Compulsões, Alterações de  humor, Relacionamentos, Luto, Conflitos familiares, Desenvolvimento pessoal, Vícios, Ansiedade e Pânico, Traumas, Fobias e Questões profissionais.</p>
                 <p>O objetivo do trabalho na clínica é possibilitar ao indivíduo a compreensão do seu modo de ser-no-mundo, abrindo assim possibilidades para nova forma de existir.</p>
-            </div>
+                </div>
             </div>
 
             <div className='conheca_meus_servicos_link'>  
