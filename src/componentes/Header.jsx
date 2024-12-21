c6964dd506446e67c4da16fb472cd5e773ea7c89
Lara <laraf.sena@gmail.com>
2024-12-21 14:53:09 -0300

    menu hamburguer

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -2,44 +2,51 @@ import React, { useState, useEffect } from "react";
 import logo from "../images/logo.png";
 import './Header.css';
 
-
-function Header(){
-
+function Header() {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollThreshold = 50; // Ajuste o valor conforme necessário
-      if (window.scrollY > scrollThreshold) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollThreshold = 50;
+            if (window.scrollY > scrollThreshold) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);  // Alterna o estado para abrir ou fechar o menu
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-
-    return(
+    return (
         <section className={`header_section ${isScrolled ? "scrolled" : ""}`}>
             <header className="header">
-                <a href="/" ><img className="logo" src={logo} alt="logo"/></a>
+                <button className="hamburger" onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                </button>
+                <a href="/" ><img className={`logo ${isMenuOpen ? "hide" : ""}`} src={logo} alt="logo" /></a>
             </header>
-            
-            <nav>
-                <ul className="menu">
-                    <li><a href="#inicio">Início</a></li>
-                     <li><a href="#sobre">Sobre</a></li>   
-                     <li><a href="#servicos">Serviços</a></li>   
-                     <li><a href="#contato">Contato</a></li>
-                </ul>
-            </nav>
+            <div className={`nav_menu_list ${isMenuOpen ? "active" : ""}`}>
+                <nav>
+                    <ul className="menu_list">
+                        <li><a href="#inicio">Início</a></li>
+                        <li><a href="#sobre">Sobre</a></li>
+                        <li><a href="#servicos">Serviços</a></li>
+                        <li><a href="#contato">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
         </section>
-    )
+    );
 }
 
 export default Header;
